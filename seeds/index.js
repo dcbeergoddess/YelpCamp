d6aa25370d6b98566b2432abaf6ac33d83dfb1c8
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-02 15:03:50 -0400

    seeded data ifrom cities and seedHelpers

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const Campground = require('../models/campground')
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers')
+const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -13,10 +15,25 @@ db.once('open', () => {
   console.log('Database Connected')
 });
 
+//GRAB RANDOM ELEMENT FROM AN ARRAY
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({title: 'purple field'});
-    await c.save();
+    //CREATE LOOP FOR DATA - 50 times - 50 cities
+    for(let i = 0; i < 50; i++){
+        //random number to pick city[from 1000 city array]
+        const random1000 = Math.floor(Math.random() * 1000);
+        //make new Campground - location: city, state
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        await camp.save()
+    }
 };
 
-seedDB();
+//seedDB returns a promise because it is an async function .then--> close connection
+seedDB().then(() => {
+    mongoose.connection.close();
+});
