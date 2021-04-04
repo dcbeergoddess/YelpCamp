371e27a487f7b439d983e6404fbc45cd663f25eb
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-03 21:10:55 -0400

    Routes to Create New Campground

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ PORT = 3000
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+
+//HOME
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -30,6 +34,16 @@ app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
 });
+//NEW FORM
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+//POST ROUTE
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`);
+})
 //SHOW
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id)
