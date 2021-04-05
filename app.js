604e80877def4a0cf835389c00756355e69497ba
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-05 14:45:39 -0400

    Added Edit-Update Functionality

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true, 
   useUnifiedTopology: true
-})
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -22,7 +23,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //HOME
 app.get('/', (req, res) => {
@@ -43,17 +45,29 @@ app.post('/campgrounds', async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`campgrounds/${campground._id}`);
-})
+});
 //SHOW
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', { campground });
-})
+});
+//UPDATE FORM
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground });
+});
+//PUT ROUTE TO UPDATE
+app.put('/campgrounds/:id', async (req, res)=> {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+  //HAD ISSUES WHEN IT WAS `campgrounds/${campground._id}`
+  res.redirect(`${campground._id}`);
+});
 
 // END OF FILE
 app.get('*', (req, res) => {
   res.send(`I DO NOT KNOW THAT PATH!!!!`)
-})
+});
 app.listen(PORT, () => {
   console.log(`LISTENING ON http://localhost:${PORT}`)
 }); 
