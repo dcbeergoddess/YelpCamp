471d67a3dd5ff65ac976fb17955790a57c06df63
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-08 19:21:15 -0400

    basic and generic error handler setup

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const engine = require('ejs-mate')
 const path = require('path');
 const mongoose = require('mongoose');
-const catchAsync = require('./utils/catchAsync')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -45,6 +46,7 @@ app.get('/campgrounds/new', (req, res) => {
 });
 //POST ROUTE
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+  if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`campgrounds/${campground._id}`);
@@ -73,9 +75,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
-//BASIC ERROR HANDLER
+//BASIC ERROR HANDLERS
+app.all('*', (req, res, next) => {
+  next( new ExpressError('Page Not Found', 404));
+  //this will pass it on the next function `app.use`
+});
+
 app.use((err, req, res, next) => {
-  res.send('OH BOY SOMETHING WENT WRONG')
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode).send(message);
 });
 //LISTENER
 app.listen(PORT, () => {
