932a43891e31e43de458817c3055b2f4d68bf6fc
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-17 19:35:44 -0400

    router for reviews setup w/ correct params merged

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+//models
 const Campground = require('./models/campground');
 const Review = require('./models/review');
-//campground router
+//routers
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -34,53 +34,16 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-//Joi MIDDLEWARE
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-};
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-};
-
-//CAMPGROUND ROUTER MIDDLEWARE
-app.use('/campgrounds', campgrounds)
+//ROUTER MIDDLEWARE
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 //HOME
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-//POST REVIEW TO CAMPGROUND ROUTE
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  campground.reviews.push(review);
-  //THERE IS A WAY TO DO NEXT TWO LINES TOGETHER
-  await review.save();
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
-//DELETE REVIEW
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-  //DESTRUCTURE FROM REQ.PARAMS
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/campgrounds/${id}`);
-}));
 //BASIC ERROR HANDLERS
 app.all('*', (req, res, next) => {
   next( new ExpressError('Page Not Found', 404));
