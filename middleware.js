e59cb554c490d88726447379bb31ed081794dcae
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-22 17:13:51 -0400

    moved campground.js middleware code to middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,34 @@
+const { campgroundSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground');
+
 module.exports.isLoggedIn = (req, res, next) => {
   // console.log("REQ.USER...", req.user);
   if(!req.isAuthenticated()) {
     req.flash('error', 'you must be signed in');
     return res.redirect('/login'); //must return this otherwise next line runs and sends error to console
+  }
+  next();
+};
+
+//JOI VALIDATION
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+};
+
+//MIDDLEWARE --> isAuthor
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if(!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that');
+    return res.redirect(`/campgrounds/${id}`); 
   }
   next();
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-
-//JOI VALIDATION
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-};
-
-//MIDDLEWARE --> isAuthor
-const isAuthor = async (req, res, next) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  if(!campground.author.equals(req.user._id)) {
-    req.flash('error', 'You do not have permission to do that');
-    return res.redirect(`/campgrounds/${id}`); 
-  }
-  next();
-};
 
 //INDEX
 router.get('/', catchAsync(async (req, res) => {
