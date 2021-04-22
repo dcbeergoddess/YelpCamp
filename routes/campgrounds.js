0217a1191adc3c5094581bf7150fa1840d487189
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-22 18:16:30 -0400

    username for reviews displaying: had issue when db had reviews not associated with a user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,14 +25,19 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 }));
 //SHOW
 router.get('/:id', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+  const campground = await Campground.findById(req.params.id).populate({
+    path: 'reviews',
+    populate: {
+      path: 'author'
+    }
+  }).populate('author');
   // console.log(campground);
   if(!campground){
     req.flash('error', 'Cannot find that campground');
     return res.redirect('/campgrounds');
   }
   //TEST THAT IT'S WORKING WITH CAMPGROUND
-  // console.log(campground);
+  console.log(campground);
   res.render('campgrounds/show', { campground });
 }));
 //UPDATE FORM
