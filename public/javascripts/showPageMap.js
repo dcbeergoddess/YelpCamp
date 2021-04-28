7ec83b132cd3b5f9f1eda8c24c7f92fe899a6905
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-28 15:40:08 -0400

    pin showing up for location on map that user inputs in form

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4 // starting zoom
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10 // starting zoom
 });
+//ADD PIN TO MAP
+new mapboxgl.Marker({ color: 'black', rotation: 45 })
+  .setLngLat(campground.geometry.coordinates)
+  .addTo(map);
