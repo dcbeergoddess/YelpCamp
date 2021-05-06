9475154041cba713537c0b0cd65521a3477735bf
Rachel Murray <dcbeergoddess@gmail.com>
2021-05-06 19:03:03 -0400

    adding escapeHTML() anywhere we add text

--- schemas.js
+++ schemas.js
@@ -27,11 +27,11 @@ module.exports.campgroundSchema = Joi.object({
   //campground is our `key` (everything is campground[title], etc)
   //it should be an object and it needs to be required
   campground: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
     // image: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required()
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML()
   }).required(),
   deleteImages: Joi.array()
 });
